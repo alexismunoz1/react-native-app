83177083a978faa07a95dd1d2c4f21645b5d6350
alexismunoz1 <munozmiguelalexis@gmail.com>
2023-09-04 13:39:30 -0300

    Add expo jest testing configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "plugin:jest/recommended",
   ],
   overrides: [
     {
